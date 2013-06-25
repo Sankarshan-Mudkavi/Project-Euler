11dffbe6d82cce9f843c7f27840c49599129a8dc
Sankarshan Mudkavi <Sankarshan.Mudkavi@gmail.com>
2013-06-25 12:13:08 -0400

    Updated readme

---
+++ 1-10/Problem-1.py
@@ -0,0 +1,25 @@
+def num_gen(n):
+	return n*(n+1)/2
+
+def num_div(num):
+	count = 0
+	for i in range(int(num/2)+1):
+		if num % (i+1) == 0:
+			count += 1
+	return count
+
+def over_fhund():
+	boo = 1
+	i = 1
+	while boo == 1:
+		k = num_gen(i)
+		if num_div(k) > 500:
+			boo = 0
+		i = i + 1
+	return i-1
+
+print over_fhund()
+
+
+
+
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 Project-Euler
 =============
 
+(I need to recommit them to github)
 Solutions to project euler problems
 That's it!
