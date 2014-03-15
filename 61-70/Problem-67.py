82264a6659b981b664b654f6777c5c5a966911d5
Sankarshan Mudkavi <Sankarshan.Mudkavi@gmail.com>
2014-03-15 18:32:31 -0400

    Added problem statements

--- 61-70/Problem-67.py
+++ 61-70/Problem-67.py
@@ -1,3 +1,16 @@
+"""By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+
+3
+7 4
+2 4 6
+8 5 9 3
+
+That is, 3 + 7 + 4 + 9 = 23.
+
+Find the maximum total from top to bottom in the tree with one-hundred rows (below).
+
+NOTE: This is a much more difficult version of Problem 18. It is not possible to try every route to solve this problem, as there are 299 altogether!
+ If you could check one trillion (1012) routes every second it would take over twenty billion years to check them all. There is an efficient algorithm to solve it. ;o)"""
 #Rather than calculate all routes, calculate the highest sum for each row from the bottom
 tree_list = [[59],
 [73, 41],
